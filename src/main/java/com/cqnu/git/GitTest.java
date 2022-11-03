51fb96fd71dfce594fae21a7eabe16df12fa16a8
we <itwluo@163.com>
2022-11-03 10:20:01 +0800

    git commit

---
+++ src/main/java/com/cqnu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cqnu.git;
+
+/**
+ * @author luowei
+ * @date 2022-11-03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+    }
+}
